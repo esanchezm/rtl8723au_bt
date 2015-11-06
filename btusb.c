292d43837f67034a1e18cbbc3527259aa885b2d7
Esteban Sánchez <esteban.sanchez@gmail.com>
2015-11-06 10:22:16 +0100

    Moved realtek dongle ids to btusb_table

--- btusb.c
+++ btusb.c
@@ -155,7 +155,6 @@ static const struct usb_device_id btusb_table[] = {
 	/* Realtek Bluetooth devices */
 	{ USB_VENDOR_AND_INTERFACE_INFO(0x0bda, 0xe0, 0x01, 0x01),
 	  .driver_info = BTUSB_REALTEK },
-
 	{ }	/* Terminating entry */
 };
 
@@ -303,31 +302,6 @@ static const struct usb_device_id blacklist_table[] = {
 	/* Other Intel Bluetooth devices */
 	{ USB_VENDOR_AND_INTERFACE_INFO(0x8087, 0xe0, 0x01, 0x01),
 	  .driver_info = BTUSB_IGNORE },
-
-	/* Realtek Bluetooth devices */
-	{ USB_VENDOR_AND_INTERFACE_INFO(0x0bda, 0xe0, 0x01, 0x01),
-	  .driver_info = BTUSB_REALTEK },
-
-	/* Additional Realtek 8723AE Bluetooth devices */
-	{ USB_DEVICE(0x0930, 0x021d), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3394), .driver_info = BTUSB_REALTEK },
-
-	/* Additional Realtek 8723BE Bluetooth devices */
-	{ USB_DEVICE(0x0489, 0xe085), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x0489, 0xe08b), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3410), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3416), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3459), .driver_info = BTUSB_REALTEK },
-
-	/* Additional Realtek 8821AE Bluetooth devices */
-	{ USB_DEVICE(0x0b05, 0x17dc), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3414), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3458), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3461), .driver_info = BTUSB_REALTEK },
-	{ USB_DEVICE(0x13d3, 0x3462), .driver_info = BTUSB_REALTEK },
-
-	/* Silicon Wave based devices */
-	{ USB_DEVICE(0x0c10, 0x0000), .driver_info = BTUSB_SWAVE },
 
 	{ }	/* Terminating entry */
 };
